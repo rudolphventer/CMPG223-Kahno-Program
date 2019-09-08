6d567e5fcdd545b801ed874639e43d6b8d804457
Sprite <kelvin.popovic@gmail.com>
2019-09-08 14:56:18 +0200

    Trouble on RestaurantMenuPage with the list and session for counting quantity.

--- Kahno-Main/RestaurantMenuPage.aspx.cs
+++ Kahno-Main/RestaurantMenuPage.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.OleDb;
 
 namespace Kahno_Main
 {
@@ -26,25 +28,64 @@ namespace Kahno_Main
         {
             Response.Redirect("ConfirmationForm.aspx");
         }
+
         protected void addToOrder(object sender, EventArgs e)
         {
-            
-            //this is the function triggered by the "add to cart" button, ItemID is the id of the selected item, do with it what you will
-
             int ItemID = Convert.ToInt32((sender as Button).CommandArgument);
+            string final = "";
+            int tempIndex = 0;
+            int qty = 1;
+
+            //code to receive data
+            string connectString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\KahnoDB.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connectString);
+            conn.Open();
+
+            string sql = "SELECT itemId, price, itemName FROM [MENUITEM] WHERE ItemId =" + ItemID;
+            SqlCommand command = new SqlCommand(sql, conn);
+            SqlDataReader dr = command.ExecuteReader();
+
+            while (dr.Read())
+            {
+                final = dr.GetValue(0).ToString() + " " + dr.GetValue(2).ToString() + " : R" + dr.GetDecimal(1).ToString() + " ";
+            }
             
             Session["itemId"] = ItemID;
             Session["orderCount" + Session["itemId"]] = (Convert.ToInt32(Session["orderCount" + Session["itemId"]]) + 1).ToString();
 
-            //testing gridview insert
-            //GridView1
+            List<int> CurrentCart = new List<int>();
+            List<int> CurrentCartQuantity = new List<int>();
+
+            try
+            {
+                CurrentCart = (List<int>)Session["list"];
+                CurrentCartQuantity = (List<int>)Session["CartQuantity"];
+            }
+            catch
+            {
+                Session["list"] = CurrentCart;
+                Session["CartQuantity"] = CurrentCartQuantity;
+            }
+
+            //calculation of quantitiy 
+            if (!CurrentCart.Contains(ItemID))
+            {
+                CurrentCart.Add(ItemID);
+                CurrentCartQuantity.Add(1);
+            }
+            else
+            {
+                tempIndex = CurrentCart.IndexOf(ItemID);
+                qty = CurrentCartQuantity[tempIndex];
+                qty = qty + 1;
+                CurrentCartQuantity.Insert(tempIndex, qty);
+            }
+
 
             //use itemId to request information from DB to get back the whole item 
-            Label1.Text = (ItemID).ToString();
+            //Label1.Text = (ItemID).ToString();
 
-            //trying the solution - send request to method in kahnoLib to get back the product line needed
-
-            string outListBox = KahnoOrderDetails.getMenuItem(ItemID);
+            string outListBox = final + CurrentCartQuantity[tempIndex]; //KahnoOrderDetails.getMenuItem(ItemID);
             ListBox1.Items.Add(outListBox);
 
         }
